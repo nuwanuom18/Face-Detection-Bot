eb939e4e8c73df8cd098539af1e25678e7f5d7b7
nuwanuom18 <nuwan.18@cse.mrt.ac.lk>
2020-06-11 23:43:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,21 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video) // to add things to the webcam video
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height:video.height}
+  faceapi.matchDimensions(canvas , displaySize)// match canvas to display size
   setInterval( async () =>{
     const detections = await faceapi.detectAllFaces(video,
       new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
       .withFaceExpressions()
       console.log(detections)
-   }, 100 // acync function
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+      canvas.getContext('2d').clearRect(0, 0, canvas.width , canvas.height) // to clear previous rect
+      faceapi.draw.drawDetections(canvas , resizedDetections) // to drow the canvas and resize
+      faceapi.draw.drawFaceLandmarks( canvas , resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvas , resizedDetections)
+    }, 100 // acync function
 
   ) // can use multiple times
 
